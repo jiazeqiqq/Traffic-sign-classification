dc1248ecbb35c6dbeeb61d8d323492df368c03cd
doge555 <pochooge@gmail.com>
2024-04-15 23:52:49 +0800

    change main
    change main

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 from data_rearrangement import rearrange_dataset, split_train_test
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--annotations_loc", type=str, default='/mnt/d/doge/GitHub_respo/Traffic-sign-classification/dataset/annotations.csv')
+    parser.add_argument("--new_image_loc", type=str, default='/mnt/d/doge/GitHub_respo/Traffic-sign-classification/dataset/new_imageset')
+    parser.add_argument("--old_image_loc", type=str, default='/mnt/d/doge/GitHub_respo/Traffic-sign-classification/dataset/images')
+    args = parser.parse_args()
+    list_of_tuple = rearrange_dataset(annotations_file_loc = args.annotations_loc, new_imageset_path=args.new_image_loc, old_imageset_path=args.old_image_loc)
+    X_train, X_test, y_train, y_test = split_train_test(list_with_tuple_bundle = list_of_tuple)
+    print('x_train:', X_train)
+    print('y_train:', y_train)
+    print('x_test:', X_test)
+    print('y_test:', y_test)
